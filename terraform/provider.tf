22d879fd5b17f0660b48f42cda748a9902dbc9c1
Prasiddha  Bista <prasiddhabista102@gmail.com>
2024-08-28 11:27:06 +1000

    feat(auto-deploy): use custom service account

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,5 +18,5 @@ provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
-  scopes  = ["https://www.googleapis.com/auth/siteverification"]
+  # scopes  = ["https://www.googleapis.com/auth/siteverification"]
 }
